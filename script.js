63679781d73fe8e846cbe5790be42593e9f16d4e
dimas <72862552+dmarsly@users.noreply.github.com>
2021-12-21 14:08:33 +0800

    Update

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-// Created by ngapain syg
-// Gada apa apa di sini
+// Created by Calviղ
 
+onload = () => {
+var sound = new Howl({
+  src: ['https://y.dimars.repl.co/Yes.mp3'],
+  autoplay: true,
+  loop: true,
+  volume: 1,
+  onend: function() {
+//    console.log('Finished!');
+  }
+});
+
+}
